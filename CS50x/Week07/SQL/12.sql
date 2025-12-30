823cf6a70076285b7912822e768367e8ccd2bc8b
Pedro Rodrigues <pdarodrigues@outlook.com>
2025-12-30 01:47:04 +0000

    Rename CS50x folders

--- CS50x/Week07/SQL/12.sql
+++
@@ -1,8 +0,0 @@
-SELECT title
-FROM movies
-JOIN stars ON movies.id = stars.movie_id
-JOIN people ON stars.person_id = people.id
-WHERE name = 'Bradley Cooper' OR name = 'Jennifer Lawrence'
-GROUP BY title
-HAVING COUNT(DISTINCT name) = 2
-;
